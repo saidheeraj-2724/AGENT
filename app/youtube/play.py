6768b2a5dfcd2ab00a117d4f9ab68579beea12b8
saidheeraj-2724 <250171601132@crescent.education>
2026-09-08 15:28:34 +0530

    Update play.py

--- app/youtube/play.py
+++ app/youtube/play.py
@@ -13,13 +13,13 @@ def get_vid(query):
     request = urllib.request.Request(
       url,
       headers={
-        "User-Agent": "Mozilla/5.0'
+        "User-Agent": "Mozilla/5.0"
       }
     )
     data = urllib.request.urlopen(
       request,
       timeout=5
-    ).read().decode("utf-8, errors = "ignore")
+    ).read().decode("utf-8", errors="ignore")
     
      ids = re.findall(
           r'"videoID":"([^"]+)"',
