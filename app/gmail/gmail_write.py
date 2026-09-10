4b8385257ee8fc80fa9f801d0fb74e884c80e853
saidheeraj-2724 <250171601132@crescent.education>
2026-09-10 14:41:36 +0530

    Update gmail_write.py

--- app/gmail/gmail_write.py
+++ app/gmail/gmail_write.py
@@ -2,9 +2,39 @@ import os
 import re
 import urllib.parse
 
-CLIENT_EMAIL = os.getenv("CLIENT_EMIAL","")
 
 KEYWORDS = (
-  "gmail","email","e-mail",
-  
+  "gmail","email","e-mail","mail"
+  "write an email","send an email","draft an email",
+  "compose an email","write mail","send mail","draft mail",
+  "compose mail"
 )
+
+def is_email_command(text):
+  text = text.lower()
+  return any(k in text for k in KEYWORDS)
+
+def extract_email(text):
+  match : re.search(r"[\w.+-]+@[\w.-]+\.\w+",text)
+  if match:
+    return match.group(0)
+
+    match = re.search(
+      r"([\w.+-]+)\s+at\s+(\w.-+)\s+dot\s+(\w+)",
+      text.lower()
+    )
+    if match:
+      return f"{match.group(1)}@{match.group(2)}.{match.group(3)}"
+
+    return ""
+
+def create_gmail_url(subject="",body="",recipitant=""):
+  params = urllib.parse.urlencode({
+    "veiw": "cm",
+    "fs": "1",
+    "to": recipitant,
+    "su": subject,
+    "body": body
+  })
+
+return f"https://mail.google.com/mail/u/0/?{params}"
