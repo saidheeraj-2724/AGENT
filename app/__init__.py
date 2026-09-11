f851b50cdf2b6317e13c2cc8570fdcc05a279183
saidheeraj-2724 <250171601132@crescent.education>
2026-09-11 09:25:52 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -16,62 +16,97 @@ from app.youtube import youtube_bp
 def create_app():
 
     app = Flask(__name__)
+
     CORS(app)
 
-    # YOUTUBE
+
+    # YouTube
     app.register_blueprint(
         youtube_bp,
         url_prefix="/youtube"
     )
 
-    # HOME
+
+    # Home
     @app.route("/")
     def home():
+
         return render_template("index.html")
+
 
     # HTML
     @app.route("/html")
     def html():
+
         return render_template("index.html")
 
-    # HEALTH
+
+    # Health
     @app.route("/health")
     def health():
+
         return jsonify({
             "status": "ok",
             "service": "Nova AI Agent"
         })
 
-    # GMAIL AI AGENT
+
+    # Gmail AI Agent
     @app.route("/agent", methods=["POST"])
     def agent():
+
         try:
-            data = request.get_json(silent=True) or {}
-            command = data.get("command", "").strip()
+
+            data = request.get_json(
+                silent=True
+            ) or {}
+
+            command = data.get(
+                "command",
+                ""
+            ).strip()
+
 
             if not command:
+
                 return jsonify({
                     "success": False,
                     "message": "Command is required"
                 }), 400
 
+
             if not is_email_command(command):
+
                 return jsonify({
                     "success": False,
                     "message": "Please give a Gmail command."
                 }), 400
 
-            recipient = extract_email(command)
 
-            email = generate_email_with_gemini(command)
+            recipient = extract_email(
+                command
+            )
+
+
+            email = generate_email_with_gemini(
+                command
+            )
+
 
             return jsonify({
+
                 "success": True,
+
                 "type": "email",
+
                 "email_generated": True,
+
                 "recipient": recipient,
+
                 "subject": email["subject"],
+
                 "body": email["body"],
+
                 "gmail_url": create_gmail_url(
                     email["subject"],
                     email["body"],
@@ -79,10 +114,13 @@ def create_app():
                 )
             })
 
+
         except Exception as e:
+
             return jsonify({
                 "success": False,
                 "message": str(e)
             }), 500
+
 
     return app
